374242b2d0c11e32e6f9df9015ad91e869230d53
mopanasiuk <mopanasiuk@lohika.com>
2023-06-16 13:21:45 +0300

    Add about page

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -1,7 +1,60 @@
-import React from "react"
+import './index.scss'
 
 export const About = () => {
     return (
-        <h1>It`s the biggest NFT service all over the world!</h1>
+        <div>
+            <h1> Welcome to our NFT Marketplace!</h1>
+
+            <h3> About Us</h3>
+
+            <p>
+                At our NFT Marketplace, we strive to provide a vibrant and secure platform for creators, collectors, and enthusiasts to explore the world of non-fungible tokens. We believe in the transformative power of digital art and aim to connect artists with a global audience, revolutionizing the way art is bought, sold, and appreciated.
+            </p>
+
+            <h3>Our Vision</h3>
+
+            <p>
+
+                Our vision is to create a thriving ecosystem that empowers artists, encourages creativity, and fosters a sense of community. We envision a future where artists can showcase their unique creations, collectors can discover and acquire rare digital assets, and enthusiasts can immerse themselves in the world of digital art and culture.
+            </p>
+
+            <h3>The Marketplace Experience</h3>
+
+            <p>
+
+                Our marketplace offers a seamless and intuitive experience for both creators and collectors. Artists can easily mint their digital artworks as NFTs and set their own terms for ownership, royalties, and licensing. We prioritize the empowerment of artists by providing them with tools and resources to protect their creations and monetize their talent.
+
+                For collectors, we offer a diverse and curated selection of NFTs spanning various categories such as art, music, virtual assets, and more. With a user-friendly interface, collectors can browse, bid on, and purchase NFTs with confidence, knowing that each transaction is secured by blockchain technology.
+            </p>
+            <h3>Security and Authenticity</h3>
+            <p>
+                We take security and authenticity seriously.Our platform leverages the power of blockchain technology to ensure the integrity of each transaction and the provenance of every NFT.Through the immutability and transparency of the blockchain, we guarantee that each digital artwork is unique, verifiable, and truly owned by the buyer.
+            </p >
+
+            <h3>Community and Engagement</h3>
+
+            <p>
+
+                We value our community of creators, collectors, and enthusiasts.We foster an inclusive and supportive environment where members can connect, collaborate, and share their passion for digital art.Our platform encourages dialogue, feedback, and active participation, nurturing a vibrant community that celebrates the creativity and innovation of NFTs.
+            </p >
+
+            < h3 > Join Us</h3 >
+            <p>
+
+                Whether you are an artist looking to showcase your work, a collector in search of unique digital assets, or simply an enthusiast curious about the NFT space, we invite you to join our marketplace.Together, let's shape the future of digital art and create a world where creativity knows no bounds.
+            </p >
+
+            < h3 > Contact Us</h3 >
+
+            <p>
+
+                If you have any questions, feedback, or inquiries, please don't hesitate to reach out to our dedicated support team. You can contact us via email, phone, or through our website. We are here to assist you and ensure that your experience on our NFT marketplace is exceptional.
+            </p >
+
+            <h5 className='mt-5'> Thank you for being a part of our journey!</h5>
+
+            <p> The NFT Marketplace Team </p>
+
+        </div >
     )
 }
